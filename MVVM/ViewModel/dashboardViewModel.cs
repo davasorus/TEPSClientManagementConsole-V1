29df07447cfbb7739c4865a4ff41a1dc77ce2032
Sean Davitt <davitts2@gmail.com>
2022-10-21 23:44:44 -0400

    added custom charting

--- MVVM/ViewModel/dashboardViewModel.cs
+++ MVVM/ViewModel/dashboardViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace TEPSClientManagementConsole_V1.MVVM.ViewModel
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Collections.Generic;
+
+namespace TEPSClientManagementConsole_V1.MVVM.ViewModel
 {
-    internal class dashboardViewModel
+    public class dashboardViewModel
     {
+        public ISeries[] Prod { get; set; }
+            = new ISeries[]
+            {
+                new PieSeries<double> { Values = new double[] { 2 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 1 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 3 } }
+            };
+
+        public ISeries[] Test { get; set; }
+            = new ISeries[]
+            {
+                new PieSeries<double> { Values = new double[] { 2 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 1 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 3 } }
+            };
+
+        public ISeries[] Train { get; set; }
+            = new ISeries[]
+            {
+                new PieSeries<double> { Values = new double[] { 2 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 1 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 3 } }
+            };
+
+
+       
+
     }
 }
