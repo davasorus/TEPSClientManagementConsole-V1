97955f5a35178024daf6534e8dd9c7a625b0945b
Sean Davitt <davitts2@gmail.com>
2022-10-23 13:31:42 -0400

    added logging and model for server error logs

---
+++ MVVM/Classes/serverErrorObj.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEPSClientManagementConsole_V1.MVVM.Classes
+{
+    public class serverErrorObj : INotifyPropertyChanged
+    {
+        private string _clientName;
+        private string _logMess;
+        private DateTime _logDateTime;
+
+        public string ClientName
+        {
+            get { return _clientName; }
+            set
+            {
+                _clientName = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string Message
+        {
+            get { return _logMess; }
+            set
+            {
+                _logMess = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return _logDateTime; }
+            set
+            {
+                _logDateTime = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //notifies the collection that the object has changed, and should be displayed
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MVVM/Model/errorLogWindowModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEPSClientManagementConsole_V1.MVVM.Classes;
+
+namespace TEPSClientManagementConsole_V1.MVVM.Model
+{
+    public class errorLogWindowModel
+    {
+        private ObservableCollection<serverErrorObj> _logs = ServerErrorLogs.Collection;
+
+        public ObservableCollection<serverErrorObj> logsCollection
+        {
+            get { return _logs; }
+            set
+            {
+                _logs = value;
+            }
+        }
+    }
+}
